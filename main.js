e71367b4b9f26460db1d55427645162105a211d2
seo-dong-hyeon <62382430+seo-dong-hyeon@users.noreply.github.com>
2020-07-22 19:13:00 +0900

    create_process

--- main.js
+++ main.js
@@ -1,36 +1,20 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
-var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url,true).query; 
-    var pathname = url.parse(_url,true).pathname; // query string을 제외한 path
-    var title = queryData.id;
-    
-    console.log(url.parse(_url,true));
-    console.log(title);
-  
-    if(pathname === '/'){
-      fs.readdir('./data', function(error, filelist){
-        fs.readFile(`data/${title}`,'utf8',function(err,description){
-          if(title === undefined){
-            title = 'Welcome';
-            description = "hello nodejs";
-          }
-          /*<ol>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-           </ol>*/
-          var list = '<ol>';
-          for(index in filelist){
-            list = list + `<li><a href="/?id=${filelist[index]}">${filelist[index]}</a></li>`;
-          }
-          list += '</ol>';
+function templateList(filelist) {
+  var list = '<ol>';
+  for (index in filelist) {
+    list = list + `<li><a href="/?id=${filelist[index]}">${filelist[index]}</a></li>`;
+  }
+  list += '</ol>';
 
-          var template = `
-          <!doctype html>
+  return list;
+}
+
+function templateHTML(title, list, description){
+  return `<!doctype html>
           <html>
           <head>
             <title>WEB1 - ${title}</title>
@@ -39,15 +23,68 @@ var app = http.createServer(function(request,response){
           <body>
            <h1><a href="/">WEB</a></h1>
            ${list}
+           <a href="/create">create</a>
            <h2>${title}</h2>
            <p>${description}</p>
           </body>
           </html>
            `
+}
+
+var app = http.createServer(function(request,response){
+    var _url = request.url;
+    var queryData = url.parse(_url,true).query; 
+    var pathname = url.parse(_url,true).pathname; // query string을 제외한 path
+    var title = queryData.id;
+    
+    console.log(url.parse(_url,true));
+
+    if(pathname === '/'){
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${title}`,'utf8',function(err,description){
+          if(title === undefined){
+            title = 'Welcome';
+            description = "hello nodejs";
+          }
+        
+          var list = templateList(filelist);
+          var template = templateHTML(title,list,description);
+
           response.writeHead(200);
           response.end(template); 
         })
       });
+    }
+    else if (pathname === '/create') {
+      fs.readdir('./data', function (error, filelist) {
+        title = "Web-create";
+        description = `<form action="http://localhost:3000/create_process" method="POST">
+         <p><input type="text" name="title" placeholder="title"></p>
+         <textarea name="desciption" placeholder="descriptions"></textarea>
+         <input type="submit">
+         </form>`;
+
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, description);
+
+        response.writeHead(200);
+        response.end(template);
+      });
+    }
+    else if(pathname === '/create_process'){
+      var body = '';
+      request.on('data',function(data){
+        body = body + data;
+      });
+
+      request.on('end',function(){
+        var ps = qs.parse(body);
+        console.log(ps);
+      });
+
+      response.writeHead(200);
+      response.end('success');
+      
     }
     else{
       response.writeHead(404);
